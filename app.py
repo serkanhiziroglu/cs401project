76debfe3c954f18f4cdca20b8fbb70e2b5efe156
SERKAN_HIZIROGLU <serkanhiziroglu2@gmail.com>
2021-06-16 08:56:08 +0300

    edit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 from flask_request_id_header.middleware import RequestID
-from datetime import datetime
+from datetime import datetime, time
 import pandas as pd
 from data import *
 import math
+import random
 
 
 app = Flask(__name__)
@@ -11,20 +12,54 @@ app = Flask(__name__)
 app.config['REQUEST_ID_UNIQUE_VALUE_PREFIX'] = 'USER-' #req ID prefix
 RequestID(app) # Add req ID
 
+user_selection_history = dict()
+agent_selection_history = dict()
+MAX_TIME = 60
+
+imageSource = "";
+imageSource2 = " ";
+
+
+P0 = 0.94
+P1 = 0.5
+P2 = 0.4
+P3 = 0.8 # mimic
+
+W = 1
+
+N = 4 # WINDOW - last n consecutive bids
+
+
+
 @app.route('/', methods=["GET"])
 def index():
 	return render_template('main.html')
 
-# AI agent ile iletisim API'yi
+
 @app.route('/getAIresponse',  methods=['POST'])
 def getAIresponse():
-	type = request.form.get('type').strip()
+	type = request.form.get('type')
+	user_id = request.form.get('user_id')
+	print("REQUEST - User ID is : " + user_id)
+	
+	init_user_history(user_id)
+	init_agent_history(user_id)
+
+	
+	time_remaining = request.form.get('timeleft')
+	print("Remainign time for user is : " + time_remaining)
 	location = request.form.get('location').strip()
 	duration = request.form.get('duration').strip()
 	transportation = request.form.get('transportation').strip()
 	print(f'type: {type}, loation: {location}, duration: {duration}, transportation: {transportation}')
-	# aiResponse = Python.run('aimodel.py', userMessage)
-	aiResponse = aiModel(type, location, duration, transportation)
+
+	add_selection_to_user_history(user_id, type, location, duration, transportation)
+
+	# target_utility = calculate_TU_hybrid(user_id, time_remaining)
+
+	aiResponse = aiModel(user_id, time_remaining)
+
+
 	return aiResponse
 
 # Get unique user ID
@@ -32,62 +67,227 @@ def getAIresponse():
 def getUserID():
 	return request.environ.get("HTTP_X_REQUEST_ID")
 
+def calculate_TU_hybrid(type, location, duration, transportation, timeleft, user_id):
+	t = float(timeleft) / MAX_TIME
+	TUtimes = calculate_TU_times(timeleft)
+	TUbehavior = calculate_TU_behavior(type, location, duration, transportation, timeleft, user_id)
+	#print(t, TUtimes, TUbehavior)
+	return (t**2 * TUtimes) + ( (1-t**2) * TUbehavior ) 
+
+def calculate_TU_times(timeleft): # Formula 2
+	t = float(timeleft) / MAX_TIME # scaled t
+	return ( (1-t**2) * P0 ) + ( 2 * (1-t) * t * P1 ) + ( t**2 * P2)
+
+def calculate_TU_behavior(type, location, duration, transportation, timeleft, user_id):
+
+
+	utility = calculate_utility(type, location, duration, transportation)
+	#print(type(past_offers[-1][4]))
+	
+
+
+	# past_offers = user_selection_history[user_id]
+	# if len(past_offers) < 1:
+	# 	return
+
+	# past_offers = user_selection_history[user_id]
+	# if len(past_offers) < 1:
+	# 	return
+
+	# most_recent_offer_utility = past_offers[-1][4] # index 4 is utility
+
+	gamma = calculate_gamma(timeleft)
+	delta_U = calculate_delta_U(user_id)
+
+	return (utility - (gamma * delta_U))
+
+
+
+def calculate_gamma(timeleft):
+	return P3 + ((float(timeleft)/MAX_TIME) * P3)
+
+def calculate_delta_U(user_id):
+	past_offers = user_selection_history[user_id]
+	delta_U = 0
+	delta_U_range = min(N, len(past_offers))
+
+	for index in range(1, delta_U_range):
+		value = W * (past_offers[len(past_offers)-index][4] - past_offers[len(past_offers)-index-1][4]) # index 4 is utility
+		delta_U += value
+
+	return delta_U
+
+def calculate_utility(type, location, duration, transportation):
+	typeU = types.loc[type]['AIdecision']
+	locationU = locations.loc[location]['AIdecision']
+	durationU = durations.loc[duration]['AIdecision']
+	transportationU = transportations.loc[transportation]['AIdecision']
+	return typeU + locationU + durationU + transportationU
+
+def add_selection_to_agent_history(user_id, type, location, duration, transportation):
+	utility = calculate_utility(type, location, duration, transportation)
+	agent_selection_history[user_id].append(
+		(type, location, duration, transportation, utility)
+	)
+
+
+
+def add_selection_to_user_history(user_id, type, location, duration, transportation):
+	utility = calculate_utility(type, location, duration, transportation)
+	user_selection_history[user_id].append(
+		(type, location, duration, transportation, utility)
+	)
+
+def init_user_history(user_id):
+	if user_id in user_selection_history:
+		return
+	else:
+		user_selection_history[user_id] = []
+
+
+def init_agent_history(user_id):
+	if user_id in agent_selection_history:
+		return
+	else:
+		agent_selection_history[user_id] = []
+
 
 # Test icin
 @app.route('/account')
 def accountPage():
 	return 'This is your account page'
 
-
-
-def aiModel(type, location, duration, transportation):
-
+def return_current_bestoffer(user_id):
+	max_utility = 0
+	selection = None
+	for t in types.index:
+		for l in locations.index:
+			for d in durations.index:
+				for transport in transportations.index:
+					utility = calculate_utility(t,l,d,transport)
+					if utility > max_utility:
+						max_utility = utility
+						selection = (t, l, d, transport)
+	add_selection_to_agent_history(user_id, selection[0], selection[1], selection[2], selection[3])
 	
-	
-
-	x= types.loc[type]['AIdecision']
-	y = math.floor((x + 4)/2)
-	z = ((types.AIdecision[types.AIdecision == y].index).to_list())
-	type = z[0]
-
-	x= locations.loc[location]['AIdecision']
-	y = math.floor((x + 4)/2)
-	z = ((locations.AIdecision[locations.AIdecision == y].index).to_list())
-	location = z[0]
-
-	x= durations.loc[duration]['AIdecision']
-	y = math.floor((x + 4)/2)
-	z = ((durations.AIdecision[durations.AIdecision == y].index).to_list())
-	duration = z[0]
-
-	x= transportations.loc[transportation]['AIdecision']
-	y = math.floor((x + 4)/2)
-	z = ((transportations.AIdecision[transportations.AIdecision == y].index).to_list())
-	transportation = z[0]
-	
-	imageSource = locations.loc[location]['Image']
+	if selection[1] == "France":
+		imageSource = typeFranceDict[selection[0]]
+		imageSource2 = transportationFranceDict[selection[3]]
+	if selection[1] == "United States":
+		imageSource = typeAmericaDict[selection[0]]
+		imageSource2 = transportationAmericaDict[selection[3]]
+	if selection[1] == "Italy":
+		imageSource = typeItalyDict[selection[0]]
+		imageSource2 = transportationItalyDict[selection[3]]
+	if selection[1] == "Japan":
+		imageSource = typeJapanDict[selection[0]]
+		imageSource2 = transportationJapanDict[selection[3]]
 
 	return {
-			"type": type,
-			"location": location,
-			"duration": duration,
-			"transportation": transportation,
-			"timestamp": datetime.now(),
-			"imageSource": imageSource
-		}
-	# text = ""
-	# if location != "TURKEY":
-	# 	my_location = "TURKEY"
-	# if text.lower() == "hello":
-	# 	return "Hi there!"
-	# elif "how are you" in text.lower():
-	# 	return "I'm fine, thank you. How are you?"
-	# elif "bye" in text.lower():
-	# 	return "Goodbye!"
-	# elif "see you" in text.lower():
-	# 	return "See you later!"
-	# else:
-	# 	return activity, duration, "TURKEY", startTime
+				"type": selection[0],
+				"location": selection[1],
+				"duration": selection[2],
+				"transportation": selection[3],
+				"timestamp": datetime.now(),
+				"imageSource": imageSource,
+				"imageSource2": imageSource2
+			}
+
+
+
+def aiModel(user_id, time_remaining):
+
+
+	max_utility = 0
+	selection = None
+	if len(agent_selection_history[user_id]) == 0:
+		return return_current_bestoffer(user_id)
+
+	t,l,d,transport,u = agent_selection_history[user_id][-1]
+	target_utility = calculate_TU_hybrid(t, l, d, transport, time_remaining, user_id)
+	offer_counter = 0
+	offer_counter_limit = 4*4*4*4
+	while offer_counter < offer_counter_limit:
+		offer_counter += 1
+		t_index = random.randint(0,3)
+		l_index = random.randint(0,3)
+		d_index = random.randint(0,3)
+		transport_index = random.randint(0,3)
+		#print (t_index, l_index, d_index, transport_index)
+		utility = calculate_utility(types.index[t_index], locations.index[l_index], 
+		durations.index[d_index], transportations.index[transport_index])
+		print("TARGET UTILITY: " + str(target_utility))
+		print("UTILITY: " + str(utility))
+		if utility >= target_utility:
+			add_selection_to_agent_history(user_id, types.index[t_index], locations.index[l_index], 
+			durations.index[d_index], transportations.index[transport_index])
+			selection = (types.index[t_index], locations.index[l_index], 
+			durations.index[d_index], transportations.index[transport_index])
+			if selection[1] == "France":
+				imageSource = typeFranceDict[selection[0]]
+				imageSource2 = transportationFranceDict[selection[3]]
+			if selection[1] == "United States":
+				imageSource = typeAmericaDict[selection[0]]
+				imageSource2 = transportationAmericaDict[selection[3]]
+			if selection[1] == "Italy":
+				imageSource = typeItalyDict[selection[0]]
+				imageSource2 = transportationItalyDict[selection[3]]
+			if selection[1] == "Japan":
+				imageSource = typeJapanDict[selection[0]]
+				imageSource2 = transportationJapanDict[selection[3]]
+
+			return {
+				"type": selection[0],
+				"location": selection[1],
+				"duration": selection[2],
+				"transportation": selection[3],
+				"timestamp": datetime.now(),
+				"imageSource": imageSource,
+				"imageSource2": imageSource2
+			}
+
+	return return_current_bestoffer(user_id)
+
+
+
+
+	
+
+
+
+
+'''
+x= types.loc[type]['AIdecision']
+y = math.floor((x + 4)/2)
+z = ((types.AIdecision[types.AIdecision == y].index).to_list())
+type = z[0]
+
+x= locations.loc[location]['AIdecision']
+y = math.floor((x + 4)/2)
+z = ((locations.AIdecision[locations.AIdecision == y].index).to_list())
+location = z[0]
+
+x= durations.loc[duration]['AIdecision']
+y = math.floor((x + 4)/2)
+z = ((durations.AIdecision[durations.AIdecision == y].index).to_list())
+duration = z[0]
+
+x= transportations.loc[transportation]['AIdecision']
+y = math.floor((x + 4)/2)
+z = ((transportations.AIdecision[transportations.AIdecision == y].index).to_list())
+transportation = z[0]
+
+imageSource = locations.loc[location]['Image']
+
+return {
+		"type": type,
+		"location": location,
+		"duration": duration,
+		"transportation": transportation,
+		"timestamp": datetime.now(),
+		"imageSource": imageSource
+	}
+'''
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- data.py
+++ data.py
@@ -2,19 +2,19 @@ import pandas as pd
 import random
 import math
 
-array= [1, 2, 3, 4]
+initValueArray= [0.10, 0.15, 0.20, 0.25]
 
 
-random.shuffle(array)
+#random.shuffle(initValueArray)
 
-print(array)
+print(initValueArray)
 
 
 locationData = {
-            'AIdecision': array,
-            'BudgetRequired':['High','High','Low','High'],
-            'Distance':['Short', 'Short', 'Short', 'Long'],
-            'Language':['French', 'Spanish', 'Greek', 'English']
+            'AIdecision': initValueArray,
+            'BudgetRequired':['High','High','High','Low'],
+            'Distance':['Long', 'Short', 'Short', 'Long'],
+            'Language':['English', 'Italian', 'French', 'Japanese']
             }
 
 locations = pd.DataFrame(locationData, index= ['United States', 'Italy', 'France', 'Japan'])
@@ -28,7 +28,72 @@ countryImages= ['https://cdnuploads.aa.com.tr/uploads/Contents/2020/03/30/thumbs
 locations['Image'] = countryImages
 
 
-random.shuffle(array)
+typeAmericaDict = {
+  "Food and Culture": "https://www.ekathimerini.com/wp-content/uploads/2021/06/statueofliberty.jpg",
+  "Activity and Entertainment": "https://expatexplore.com/blog/wp-content/uploads/2018/11/New-Years-Eve-New-York-City-Travel-America-Expat-Explore.jpg",
+  "Nature and Adventure": "https://www.mercurynews.com/wp-content/uploads/2019/11/SJM-Z-DelicateArchFall-1129.jpg",
+  "Beach": "https://i.insider.com/5771930491058427008cbe6d?width=1000&format=jpeg&auto=webp"
+}
+
+typeFranceDict = {
+  "Food and Culture": "https://www.eutouring.com/statues_in_paris_m15_DSC08734_lrg.jpg",
+  "Activity and Entertainment": "https://i2.wp.com/thegoodlifefrance.com/wp-content/uploads/2018/01/nice-carnival.jpg?ssl=1",
+  "Nature and Adventure": "https://www.responsiblevacation.com/imagesClient/S_152753.jpg",
+  "Beach": "https://www.themediterraneantraveller.com/wp-content/uploads/2018/03/palombaggia-beach-corsica-france.jpeg"
+}
+
+typeItalyDict = {
+  "Food and Culture": "https://www.commisceo-global.com/images/easyblog_articles/1817/leaning-tower-of-pisa.jpg",
+  "Activity and Entertainment": "https://www.lifeinitaly.com/wp-content/uploads/2018/08/jeannette.bari_.jpg",
+  "Nature and Adventure": "https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2015/01/gongon872681.jpg",
+  "Beach": "https://a.cdn-hotels.com/gdcs/production193/d1872/bb6c4092-88a4-4754-9d2f-2a2ab2700ed8.jpg"
+}
+
+typeJapanDict = {
+  "Food and Culture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqYQ1ukp6kFXfQvRwecs9HymvMEs35esRDA&usqp=CAU",
+  "Activity and Entertainment": "https://discovery.cathaypacific.com/wp-content/uploads/2017/06/Hero-japan-Events-Festival-1.jpg",
+  "Nature and Adventure": "https://www.nationalgeographic.com/content/dam/expeditions/destinations/asia/active/japan-adventure.ngsversion.1511365177185.adapt.1900.1.jpg",
+  "Beach": "https://www.planetware.com/wpimages/2020/06/japan-best-beaches-okinawa-beaches.jpg"
+}
+
+
+transportationAmericaDict = {
+  "Train": "https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2018/3/12/0/CI-Amtrak_California-Zephyr.jpg.rend.hgtvcom.616.462.suffix/1520875943365.jpeg",
+  "Car": "https://blog.padi.com/wp-content/uploads/2016/08/shutterstock_358230071.jpg",
+  "Cruise": "https://www.gannett-cdn.com/-mm-/f02a958f8e3aefc58c09599f576b304b0d6ee476/c=0-105-1023-683/local/-/media/2018/04/23/USATODAY/USATODAY/636601076574988475-001.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp",
+  "Caravan": "https://images.squarespace-cdn.com/content/v1/5ca3e1daca525b6d60ba7012/1587238435095-DWQ0G1L0LJBQMMUNJSYK/ke17ZwdGBToddI8pDm48kHem505q6McQd8XRhQc9zkRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxpEURhgSBEh9ILe0HKIG3CcOCklsxWwam3CdWF6Xw7jwVv5P97BEtB5XaVnmvMtTA/campervan+rentals+in+the+USA.jpeg?format=750w"
+}
+
+transportationFranceDict = {
+  "Train": "https://www.tripsavvy.com/thmb/am1CzuipHlAQ2jVTnNIfB88fnuA=/2121x1193/smart/filters:no_upscale()/GettyImages-527073131-5c3396b6c9e77c0001249aee.jpg",
+  "Car": "https://travelfrancebucketlist.com/wp-content/uploads/2019/09/Travel-France-by-Car-1024x683.jpg",
+  "Cruise": "https://world-traveled.com/wp-content/uploads/2021/05/55922576-41742700.jpgv1619803540.jpeg",
+  "Caravan": "https://st.focusedcollection.com/14026668/i/650/focused_268296548-stock-photo-france-caravan-roadside.jpg"
+}
+
+transportationItalyDict = {
+  "Train": "https://www.italymagazine.com/sites/default/files/styles/800xauto/public/feature-story/leader/bigstock-italy-cinque-terre-train-at-22461182.jpg?itok=buR2GwqD",
+  "Car": "https://i2.wp.com/passionpassport.com/wp-content/uploads/2017/10/italy-road-trip-davide-oricchi-480x600.jpg?resize=480%2C600",
+  "Cruise": "https://avoid-crowds.com/wp-content/uploads/2019/09/civitavecchia-1400060_1920.jpg",
+  "Caravan": "https://www.wandering-bird.com/wp-content/uploads/2019/10/great-dolomites-road-motorhome.jpg"
+}
+
+transportationJapanDict = {
+  "Train": "https://the-shooting-star.com/wp-content/uploads/2018/05/hans-johnson-Kyushu.jpg",
+  "Car": "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002785/img/basic/a0002785_main.jpg?20201222155816&q=80&rw=750&rh=536",
+  "Cruise": "https://pbcruise.com/assets/images/japangrace/sec01_01.jpg",
+  "Caravan": "https://a3.cdn.japantravel.com/photo/25363-135348/800!/travel-japan-by-campervan-135348.jpg"
+}
+
+
+
+
+
+
+
+
+
+#random.shuffle(initValueArray)
 
 print(locations)
 
@@ -42,28 +107,28 @@ print(locations)
 
 
 typeData = {
-            'AIdecision':array
+            'AIdecision':initValueArray
             }
 
-types = pd.DataFrame(typeData, index= ['Food and Culture', 'Activity and Entartainment',
+types = pd.DataFrame(typeData, index= ['Food and Culture', 'Activity and Entertainment',
  'Nature and Adventure', 'Beach'])
 
-random.shuffle(array)
+#random.shuffle(initValueArray)
 
 print(types)
 
 durationData= {
-            'AIdecision':array
+            'AIdecision':initValueArray
             }
 
 durations = pd.DataFrame(durationData, index= ['5 Days', '7 Days', '10 Days', '15 Days'])
 
-random.shuffle(array)
+random.shuffle(initValueArray)
 
 print(durations)
 
 transportationData= {
-            'AIdecision':array
+            'AIdecision':initValueArray
             }
 
 transportations = pd.DataFrame(transportationData, index= ['Train', 'Car', 'Cruise', 'Caravan'])
